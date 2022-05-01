5ace35e93ccedae063f8b1e36b88642115b4cf43
Ticianne <ticidias@gmail.com>
2022-04-30 22:34:21 -0300

    change: changing Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header/Header"
+import GlobalStyle from './Style/GlobalStyle'
+
 function App() {
 
   return (
     <div>
+      <GlobalStyle/>
+      <Header/>
       <h1>To Do List</h1>
     </div>
   )
